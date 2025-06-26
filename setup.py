8644c26ae879bab31436422b5411f012597f8e82
Harry Li <lidatou0708@gmail.com>
2025-06-26 02:34:45 +0000

    merging gutinstinct scripts

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        'hest @ git+https://github.com/MahmoodLab/HEST.git@main',  # use specific commit or tag if needed
+    ],
 )
--- src/SQUIDp/util.py
+++ src/SQUIDp/util.py
@@ -1,5 +1,7 @@
 import os
 import os.path as osp
+import pandas as pd
+from typing import List
 
 def auto_expand(path):
     """
@@ -8,3 +10,13 @@ def auto_expand(path):
     if '~' in path:
         return osp.expanduser(path)
     return path
+
+def get_ids(meta_df,  tissues: List[str], 
+            species: str = 'Homo sapiens', st_technology: str = 'Xenium',):
+    """
+    Returns the sample ids according to the filter specified
+    """
+    meta_df = meta_df[meta_df['species'] == species]
+    meta_df = meta_df[meta_df['st_technology'] == st_technology]
+    meta_df = meta_df[meta_df['tissue'].isin(tissues)]
+    return meta_df['id'].values
